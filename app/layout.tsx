71d5c9e12c824ad13745b84d4f8522fdb9f930f5
PhoebeHoku <95447747+PhoebeHoku@users.noreply.github.com>
2024-12-12 15:26:06 -0500

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,20 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+const sauceTwo = localFont({
+    src: "./fonts/OpenSauceTwo-Regular.ttf",
+    variable: "--font-sauce-two-regular",
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${sauceTwo.variable} antialiased`}
       >
         {children}
       </body>
